5f2d66e46edd0adfcbc22dc07c3e386872ee3696
Catalina McQuade <catalinamcquade@gmail.com>
2020-11-12 12:30:29 -0500

    Added more routes for cart and some styling

--- server/api/cart.js
+++ server/api/cart.js
@@ -11,4 +11,38 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+// POST item to cart
+router.post('/', async (req, res, next) => {
+    try {
+        const item = await Order_Item.create(req.body)
+        res.json(item)
+    } catch (err) {
+        next(err)
+    }
+})
+
+// PUT (change) item in cart
+router.put('/:Order_ItemId', async (req, res, next) => {
+    try {
+        const item = await Order_Item.findByPk(req.params.Order_ItemId)
+        await item.update(req.body)
+        res.json(item)
+    } catch (err) {
+        next(err)
+    }
+})
+
+// DELETE item from cart
+router.delete('/:Order_ItemId', async (req, res, next) => {
+    try {
+        const id = req.params.Order_ItemId
+        await Order_Item.destroy({
+            where: { id }
+        })
+        res.status(204).end()
+    } catch (err) {
+        next(err)
+    }
+})
+
 module.exports = router
