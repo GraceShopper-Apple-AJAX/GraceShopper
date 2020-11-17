d8a1574bb268bf06872c8c728b0e98df29aa48c9
Dana Zhang <danadaners@gmail.com>
2020-11-17 17:13:57 -0500

    cart changes

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,17 +2,54 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import {Link} from 'react-router-dom';
+import {Button, Confirm} from 'semantic-ui-react';
+
+import SingleProduct from './SingleProduct';
+import {updateCart, fetchCart, deleteCart, deleteFromCart} from '../store/cart';
 
 import './styles/Cart.css';
 
 export class Cart extends React.Component {
-  //   constructor(props) {
-  //     super(props);
+  constructor(props) {
+    super(props);
+
+    this.state = {open: false};
+  }
+
+  show = () => this.setState({open: true});
+
+  handleConfirm = () => this.setState({result: 'confirmed', open: false});
+  handleCancel = () => this.setState({result: 'cancelled', open: false});
+
+  //if user doesn't have items in cart, can't checkout AND the items box says "your cart is empty"
+
+  //delete button removes all instances from the cart, or can change quantity with + - buttons
+
+  //need to show and update cart total + shipping, number of items
+
+  //calculate total price + shipping
+
+  //get cart based on user id or sessionId if not logged in?
+  componentDidMount() {
+    if (this.props.userId) {
+      //get cart based on user id
+    } else {
+      this.props.fetchCart(this.props.match.params.orderId);
+    }
+  }
+
+  // async clearCartItems(cart){
+  //   try{
+  //     await this.props.deleteCartItems(cart);
+  //     this.props.fetchCart
+  //   } catch(err){
+  //     console.error(err)
   //   }
+  // }
 
   render() {
-    const order = this.props.order;
-    const orderItem = this.props.order_items;
+    const cartProducts = this.props;
+
     return (
       <div id="cartpage-wrapper">
         <div id="carttitle">
@@ -20,58 +57,83 @@ export class Cart extends React.Component {
           <Link to="/products">Continue Shopping</Link>
         </div>
 
-        <div id="cart-wrap">
-          <div id="cartitem-wrapper">
-            <table id="itemtable">
-              <tbody>
-                <tr>
-                  <th width="100px"></th>
-                  <th width="120px" className="centeralign">
-                    Item Name
-                  </th>
-                  <th>Size</th>
-                  <th>Price</th>
-                  <th width="40px" className="rightalign">
-                    Qty
-                  </th>
-                  <th></th>
-                </tr>
-                <tr>
-                  <td>
-                    <img src="https://bit.ly/2Uw2GZ1" id="checkout-thumb"></img>
-                  </td>
-                  <td>item nameeeee</td>
-                  <td>Pint</td>
-                  <td>$3.99</td>
-                  <td>- 1 +</td>
-                  <td>X</td>
-                </tr>
-              </tbody>
-            </table>
+        <div id="clear-cart-button">
+          <button>Clear Cart</button>
+
+          <div id="cart-wrap">
+            <div id="cartitem-wrapper">
+              {this.cartProducts.length ? (
+                <div>
+                  <table id="itemtable">
+                    <tbody>
+                      <tr>
+                        <th width="100px"></th>
+                        <th width="120px" className="centeralign">
+                          Item Name
+                        </th>
+                        <th>Size</th>
+                        <th>Price</th>
+                        <th width="40px" className="rightalign">
+                          Qty
+                        </th>
+                      </tr>
+                      {cartProducts.map((product) => (
+                        <tr key={product.id}>
+                          <td id="checkout-thumb">{product.imageUrl}</td>
+                          <td>{product.name}</td>
+                          <td>{product.selected_size}</td>
+                          <td>{product.price}</td>
+                          <td>-{product.quantity}+</td>
+                          <td>
+                            <button
+                              onClick={() =>
+                                this.props.deleteFromCart(
+                                  quantity,
+                                  selected_size,
+                                  productId
+                                )
+                              }
+                            >
+                              x
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              ) : (
+                'Your cart is empty.'
+              )}
+            </div>
           </div>
 
           <div id="summary-wrapper">
             <h4>Order Summary:</h4>
-            <div>2 Items</div>
+            <div>{quantity}Items</div>
             <table id="summary-table">
               <tbody>
                 <tr>
                   <th>Item Total</th>
-                  <td>$15.00</td>
+                  <td>NUMBER OF CART ITEMS</td>
                 </tr>
+
                 <tr>
                   <th>Shipping</th>
-                  <td>$35.00</td>
+                  <td>{cart.total >= 100 ? 'Free' : '$35.00'}</td>
                 </tr>
                 <tr>
                   <th>Total</th>
-                  <td>$50.00</td>
+                  <td>CART PRICE + SHIPPING</td>
                 </tr>
               </tbody>
             </table>
 
             <Link to="/checkout">
-              <button id="checkout-button" type="submit">
+              <button
+                id="checkout-button"
+                disabled={this.state.cartProducts.length ? '' : 'disabled'}
+              >
                 Checkout
               </button>
             </Link>
@@ -82,12 +144,26 @@ export class Cart extends React.Component {
   }
 }
 
-const mapState = (state) => {
-  return {};
+const mapStateToProps = (state) => {
+  return {
+    cart: state.cart,
+    cartProducts: state.cart.order_items,
+    userId: state.user.id,
+  };
 };
 
-const mapDispatch = (dispatch) => {
-  return {};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchCart: (cart) => dispatch(fetchCart(cart)),
+
+    updateCart: (quantity, selected_size, productId) =>
+      dispatch(updateCart(quantity, selected_size, productId)),
+
+    deleteFromCart: (quantity, selectedSize, productId) =>
+      dispatch(deleteFromCart(quantity, selectedSize, productId)),
+
+    deleteCart: (cart) => dispatch(deleteCart(cart)),
+  };
 };
 
-export default withRouter(connect(mapState, mapDispatch)(Cart));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Cart));
--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,10 +1,68 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {fetchSingleProduct} from '../store/singleProduct';
+import {updateCart} from '../store/cart';
 
 class SingleProduct extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: 0,
+      selected_size: null,
+      totalPrice: 0,
+      itemPrice: 0,
+      showAdded: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.addToCart = this.addToCart.bind(this);
+  }
   componentDidMount() {
     this.props.fetchSingleProduct(this.props.match.params.productId);
+  }
+
+  //add / decrease quantity
+  incrementQuantity = () => {
+    this.setState((prevState) => {
+      return {
+        quantity: prevState.quantity + 1,
+      };
+    });
+  };
+
+  decrementQuantity = () => {
+    this.setState((prevState) => {
+      if (prevState.quantity > 0) {
+        return {
+          quantity: prevState.quantity - 1,
+        };
+      } else {
+        return null;
+      }
+    });
+  };
+
+  //add total Price
+  addTotal = (itemPrice) => {
+    this.setState((prevState) => {
+      return {
+        totalPrice: prevState.totalPrice + itemPrice,
+      };
+    });
+  };
+
+  //add to cart, if successful show a component that says "[number][item] added to your cart!"
+  //ALSO clear quantity/price after adding to cart?
+  //ADD TO CART DECLINED ONLY IF there is another item with the same id AND size
+  addToCart = () => {
+    this.setState({
+      showAdded: true,
+    });
+  };
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   }
 
   getStatusMessage = (product) => {
@@ -17,49 +75,86 @@ class SingleProduct extends React.Component {
     }
   };
 
+  getSize(size, product) {
+    if (size === 'Scoop') {
+      return (
+        <>
+          ${product.scoop_price}{' '}
+          <input value={this.state.quantity} onChange={this.handleChange} />
+        </>
+      );
+    } else if (size === 'Pint') {
+      return (
+        <>
+          ${product.pint_price}{' '}
+          <input value={this.state.quantity} onChange={this.handleChange} />
+        </>
+      );
+    } else {
+      return (
+        <>
+          ${product.tub_price}{' '}
+          <input value={this.state.quantity} onChange={this.handleChange} />
+        </>
+      );
+    }
+  }
+
   render() {
     const product = this.props.product;
     if (product === null) {
       return <h3>There was a problem fetching this product!</h3>;
     }
 
+    const addQuantity = <button onClick={this.incrementQuantity}>+</button>;
+    const decreaseQuantity = (
+      <button onClick={this.decrementQuantity}>-</button>
+    );
+
     return (
       <div className="page-wrap">
         <div className="component-wrap">
           <div className="single-product-details">
+            {this.state.showAdded ? (
+              <div>
+                added {this.state.quantity}
+                {product.name}
+                {this.state.quantity > 1 ? 's' : null} to cart
+              </div>
+            ) : null}
             <h1>{product.name}</h1>
             <img
               src={product.imageUrl}
               className="single-product-image"
               height="200"
             />
-            <h3>Prices:</h3>
-            <ul>
-              <li>
-                Scoop - ${product.scoop_price} (
-                {product.status === 'in_stock'
-                  ? product.scoop_quantity
-                  : undefined}{' '}
-                remaining)
-              </li>
-              <li>
-                Pint - ${product.pint_price} (
-                {product.status === 'in_stock'
-                  ? product.pint_quantity
-                  : undefined}{' '}
-                remaining)
-              </li>
-              <li>
-                Tub - ${product.tub_price} (
-                {product.status === 'in_stock'
-                  ? product.tub_quantity
-                  : undefined}{' '}
-                remaining)
-              </li>
-            </ul>
             <h3>Description:</h3> {product.description}
+            <h3>Choose Size and Quantity:</h3>
+            <select id="dropdown" onChange={this.handleChange}>
+              <option value="scoop">Scoop</option>
+              <option value="pint">Pint</option>
+              <option value="tub">Tub</option>
+            </select>
+            <h3>
+              {decreaseQuantity}Quantity{addQuantity}
+            </h3>
+            <h3>Number of Items:</h3> {this.state.quantity}
+            <h3>Total Price:</h3> {this.state.totalPrice}
+            <button
+              type="button"
+              onClick={() =>
+                updateCart(
+                  this.state.quantity,
+                  this.state.selected_size,
+                  this.props.product.id
+                )
+              }
+            >
+              Add to Cart
+            </button>
             {product.status === 'in_stock' ? undefined : (
               <>
+                {' '}
                 <h3> Status:</h3> {this.getStatusMessage(product)}
               </>
             )}
@@ -79,6 +174,8 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchSingleProduct: (id) => dispatch(fetchSingleProduct(id)),
+    updateCart: (quantity, selected_size, productId) =>
+      dispatch(updateCart(quantity, selected_size, productId)),
   };
 };
 
