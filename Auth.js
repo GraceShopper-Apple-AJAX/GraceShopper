69fcd6f62fc9638a68ab9f18e7910a985ce00d4f
Catalina McQuade <catalinamcquade@gmail.com>
2020-11-17 22:48:07 -0500

    Added some styling to MyAccount

---
+++ Auth.js
@@ -0,0 +1,94 @@
+import auth0js from 'auth0-js';
+import history from '../history';
+
+const isBrowser = typeof window !== 'undefined';
+
+export default class Auth {
+  auth0 = new auth0.WebAuth({
+    domain: process.env.Auth_Domain,
+    clientID: process.env.Auth_ClientId,
+    redirectUri: process.env.Auth_Callback,
+    responseType: 'token id_token',
+    scope: 'openid profile email'
+  });
+
+  constructor() {
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.getAccessToken = this.getAccessToken.bind(this);
+    this.scheduleRenewal()
+  }
+
+  login() {
+    this.auth0.authorize();
+    this.scheduleRenewal();
+  }
+
+  logout() {
+    // Clear access token and ID token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    localStorage.removeItem('user');
+    clearTimeout(this.tokenRenewalTimeout);
+    history.replace("/")
+  }
+
+  handleAuthentication() {
+    if (typeof window !== 'undefined') {
+      this.auth0.parseHash((err, authResult) => {
+        if (authResult && authResult.accessToken && authResult.idToken) {
+          this.setSession(authResult);
+          history.replace("/")
+        } else if (err) {
+          history.replace("/")
+          console.log(err);
+        }
+      });
+    }
+  }
+
+  isAuthenticated() {
+    // Check whether the current time is past the
+    // access token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
+  setSession(authResult) {
+    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+    this.auth0.client.userInfo(authResult.accessToken, (err, user) => {
+        if (err) {
+            console.log(err);
+        } else {
+            localStorage.setItem('user', JSON.stringify(user));
+            history.replace('/');
+        }
+    })
+  }
+
+  getUser() {
+    if (localStorage.getItem('user')) {
+      return JSON.parse(localStorage.getItem('user'));
+    }
+  }
+
+  getUserName() {
+    if (this.getUser()) {
+      return this.getUser().name;
+    }
+  }
+
+  getAccessToken() {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+      throw new Error('No access token found');
+    }
+    return accessToken;
+  }
+}
