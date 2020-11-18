b66ecabd3544392b9dbcd0bc2cb1382b99a506d5
Prakruti PK <prakruti.pk@gmail.com>
2020-11-17 20:27:25 -0500

    Payment.js restored

--- client/components/Payment.js
+++ client/components/Payment.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
 import {Form, Button} from 'semantic-ui-react';
+import StripeCheckout from 'react-stripe-checkout';
 
-class Payment extends Component {
-  constructor(props) {
-    super(props);
-  }
+class Payment extends React.Component {
+  onToken = (token, addresses) => {};
 
   saveAndContinue = (e) => {
     e.preventDefault();
@@ -17,31 +16,19 @@ class Payment extends Component {
   };
 
   render() {
-    const {values, handleSubmit} = this.props;
-
     return (
-      <Form color="blue">
-        <h1 className="ui centered">Enter Personal Details</h1>
-        <Form.Field>
-          <label>Credit card type</label>
-          <input
-            placeholder="Card Type"
-            onChange={this.props.handleChange('cc_type')}
-            defaultValue={values.cc_type}
-          />
-        </Form.Field>
-        <Form.Field>
-          <label>Card number</label>
-          <input
-            placeholder="Credit Card Number"
-            onChange={this.props.handleChange('cc_number_secure')}
-            defaultValue={values.cc_number_secure}
-          />
-        </Form.Field>
-
-        <Button onClick={this.back}>Back</Button>
-        <Button onClick={this.saveAndContinue}>Next</Button>
-      </Form>
+      <StripeCheckout
+        amount={900}
+        billingAddress
+        description="Sugar Shack"
+        image="https://cdn.vox-cdn.com/thumbor/ilgObBdqZ27E-Ozyc-Cr7CEdoVs=/0x0:2000x1200/1200x800/filters:focal(654x276:974x596)/cdn.vox-cdn.com/uploads/chorus_image/image/64824148/Cone_on_Cone_2.0.jpg"
+        locale="auto"
+        name="Sugar Shack"
+        stripeKey="pk_test_51Hoe2ZHmMAEDSKHEfIyqTlWOod5liaSmREXvgs7Fs9y7FMpGy5XaiV71UtOB7AWJ4dhBvgKnsMg0nHlbzMlTYCV900a9xYsuGD"
+        token={this.onToken}
+        zipCode
+        label="Pay with 💳"
+      />
     );
   }
 }
